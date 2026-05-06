1bcb58320192c816f0a3c74e4dadddee6cbef148
kirameku26 <kirameku26@gmail.com>
2026-05-06 14:30:50 +0900

    ユーザースクリプトをアップデート

--- README.md
+++ README.md
@@ -1,9 +1,6 @@
-# README
-# Change-saizeria-to-saizeriya
 [![GreasyFork](https://img.shields.io/badge/GreasyFork-install-orange)](https://greasyfork.org/ja/scripts/576801-change-saizeria-to-saizeriya)
 [![GitHub](https://img.shields.io/badge/GitHub-Repository-green)](https://github.com/kirameku26/Change-saizeria-to-saizeriya)
-## このスクリプトについて
-### サイゼリヤと書かれていた場合にその部分の文字をサイゼリヤに変える処理を1秒ごとにしています。何か問題が起きても自己責任です
-あとinputとtextarea属性(入力欄に使われている属性)は置き換えません<br>
+### サイゼリアと書かれていた場合にその部分の文字をサイゼリヤに変える処理を1秒ごとにしています。何か問題が起きても自己責任です
+あとinputとtextarea属性(入力欄に使われている属性)とXのポスト入力欄は置き換えません<br>
 問題があったら<a href="https://github.com/kirameku26/Change-saizeria-to-saizeriya/issues">ここ</a>へ<br>
 制作にAIを使用しました
--- UserScript.js
+++ UserScript.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         Change saizeria to saizeriya
 // @namespace    https://github.com/kirameku26
-// @version      1.0
-// @description  サイゼリヤをサイゼリヤに変えます
+// @version      1.1.0
+// @description  サイゼリアをサイゼリヤに変えます
 // @author       kirameku
-// @supportURL   https://github.com/kirameku26/Change-saizeria-to-saizeriya/issues
 // @match        *://*/*
+// @supportURL   https://github.com/kirameku26/Change-saizeria-to-saizeriya/issues
 // @grant        none
 // @license      MIT
 // ==/UserScript==
@@ -13,17 +13,20 @@
 (function() {
     'use strict';
     setInterval(() => {
-        const searchKey = "サイゼリヤ";
-        const fromWord = "サイゼリヤ";
+        const fromWord = "サイゼリア";
         const toWord = "サイゼリヤ";
 
-        // ページ全体の要素を一つずつチェックする関数
         const walk = (node) => {
             // input要素やtextarea要素は無視する
             if (node.nodeName === 'INPUT' || node.nodeName === 'TEXTAREA') return;
+                // ページ全体の要素を一つずつチェックする関数
+            if (node.nodeType === 1) { // ELEMENT_NODE
+                const ariaLabel = node.getAttribute('aria-label');
+                if (ariaLabel === 'ポスト本文') return;
+            }
 
             if (node.nodeType === 3) { // テキストノード（実際の文字部分）の場合
-                if (node.nodeValue.includes(searchKey)) {
+                if (node.nodeValue.includes(fromWord)) {
                     node.nodeValue = node.nodeValue.replaceAll(fromWord, toWord);
                 }
             } else {
